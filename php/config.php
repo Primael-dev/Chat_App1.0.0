6e51d30f6a588853a256a9a3ef7b3f4df7b54ef4
Primael BANKOLE <bankolewilli@gmail.com>
2025-07-18 21:07:44 +0100

    ajout de la bdd distante

--- php/config.php
+++ php/config.php
@@ -1,8 +1,8 @@
 <?php
-  $hostname = "localhost";
-  $username = "root";
-  $password = "";
-  $dbname = "chatapp";
+  $hostname = "nue.domcloud.co";
+  $username = "chatapp";
+  $password = "P8sl((3JtpIQ4_4Jv4";
+  $dbname = "chatapp_db";
 
   $conn = mysqli_connect($hostname, $username, $password, $dbname);
   if(!$conn){
